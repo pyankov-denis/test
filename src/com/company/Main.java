4204715258d9e71ff867e57c3b5c54d1a74204bd
user ITcube <ITcube67@gmail.com>
2022-01-19 16:28:16 +0300

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Main {
 
@@ -18,9 +20,8 @@ public class Main {
         textField.setBounds(10,50,250,30);
         frame.add(textField);
         JLabel label = new JLabel("Надпись");
-        label.setBounds(10,190, 250,30);
+        label.setBounds(10,90, 250,30);
         frame.add(label);
-        frame.setVisible(true);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -28,5 +29,44 @@ public class Main {
                 JOptionPane.showMessageDialog(null, "Вы нажали на кнопку", "Предупреждение", JOptionPane.WARNING_MESSAGE, icon);
             }
         });
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Icon icon2 = new ImageIcon("D:/icon.jpg");
+                JOptionPane.showMessageDialog(null, "Вы нажали на интер", "Партия гордится тобой", JOptionPane.WARNING_MESSAGE, icon2);
+            }
+        });
+        label.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                label.setText("Клавиша опущена");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                label.setText("Клавиша поднята");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                label.setText("курсор");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    System.out.println("Ошибка");
+                }
+                label.setText("курсор убрали");
+            }
+        });
+        frame.setVisible(true);
     }
 }
