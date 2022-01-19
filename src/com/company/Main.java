f7c5d8fe34ac432e373a4f9dda1de9cab8a5e3f6
user ITcube <ITcube67@gmail.com>
2022-01-19 16:01:04 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ещё один проект");
+        frame.setLayout(null);
+        frame.setBounds(100,100,600,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton button = new JButton("Кнопка");
+        button.setBounds(10,10,250,30);
+        frame.add(button);
+        JTextField textField = new JTextField();
+        textField.setBounds(10,50,250,30);
+        frame.add(textField);
+        JLabel label = new JLabel("Надпись");
+        label.setBounds(10,90, 250,30);
+        frame.add(label);
+        frame.setVisible(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Icon icon = new ImageIcon("D:/icon.jpg");
+                JOptionPane.showMessageDialog(null, "Вы нажали на кнопку", "Предупреждение", JOptionPane.WARNING_MESSAGE, icon);
+            }
+        });
+    }
+}
